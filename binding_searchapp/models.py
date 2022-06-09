05451c9b144608b4ebd3fb324b08310adb15b428
madina <madinaa.1467@gmail.com>
2022-06-10 01:09:53 +0600

    copy from chempl.io project

--- binding_searchapp/models.py
+++ binding_searchapp/models.py
@@ -1,38 +1,18 @@
 # Import necessary modules
 
 from django.db import models
-from django.urls import  reverse
-
-
-class Booktype(models.Model):
-    btype = models.CharField(max_length=100, unique=True)
-    class Meta:
-        ordering=('btype',)
-
-
-# Create model gor book
-class Book(models.Model):
-    binding_name = models.CharField(max_length=150)
-    author_name = models.CharField(max_length=150)
-    type = models.ForeignKey(Booktype, on_delete=models.CASCADE)
-    price = models.FloatField()
-    publication = models.CharField(max_length=100)
-
-    class Meta:
-        ordering=('binding_name',)
-    def __str__(self):
-        return self.binding_name
-
-    def get_url(self):
-       return reverse('binding_detail', args=[self.id])
+# from django import models
+# from django_rdkit import models
+# from django_rdkit.models import *
+from django.contrib.postgres.fields import ArrayField
+from django import forms
+from django.urls import reverse
 
 BINDING_MODEL_TYPES = [
-    ('m1', 'Unspecified'),
-    ('m2', 'Tutorial'),
-    ('m3', 'Research'),
-    ('m4', 'Review'),
+    ('m1', 'bdtdc_ki_shrinked_test'),
+    ('m2', 'bdtdc_kd_shrinked_test'),
+    ('m3', 'bdtdc_ic50_shrinked_test'),
 ]
-
 
 class Binding(models.Model):
     drug = models.TextField()
@@ -41,12 +21,51 @@ class Binding(models.Model):
     gcnnet_bindingdb_ic50 = models.FloatField()
     gcnnet_bdtdc_ic50 = models.FloatField()
     model = models.CharField(max_length=100)
-    type = models.CharField(max_length=2, choices=BINDING_MODEL_TYPES, default='m1')
+
+    # type = models.CharField(max_length=2, choices=BINDING_MODEL_TYPES, default='m1')
+    #
+    # def type_to_string(self):
+    #     if self.type == 'm1':
+    #         return 'bdtdc_ki_shrinked_test'
+    #     elif self.type == 'm2':
+    #         return 'bdtdc_kd_shrinked_test'
+    #     elif self.type == 'm3':
+    #         return 'bdtdc_ic50_shrinked_test'
 
     class Meta:
-        ordering=('drug',)
+        ordering = ('drug',)
+
     def __str__(self):
         return self.drug
 
     def get_binding_details(self):
-       return reverse('binding_detail', args=[self.id])
+        return reverse('binding_detail', args=[self.id])
+
+
+class BindingForm(forms.ModelForm):
+    class Meta:
+        model = Binding
+        fields = (
+            'drug', 'target', 'gcnnet_bindingdb_ic50', 'gcnnet_bdtdc_ic50', 'model'
+        )
+
+
+# class Entry(models.Model):
+#     m1 = ArrayField(
+#         model_container=Binding,
+#         model_form_class=BindingForm)
+#
+#     m2 = ArrayField(
+#         model_container=Binding,
+#         model_form_class=BindingForm)
+#
+#     m3 = ArrayField(
+#         model_container=Binding,
+#         model_form_class=BindingForm)
+
+    # objects = models.DjongoManager()
+
+# class BindingModels(models.Model):
+#     m1 = models.ForeignKey(Binding) # = JSONField()
+#     m2 = models.ForeignKey(Binding)
+#     m3 = models.ArrayField(models.ArrayField(Binding))
--- binding_searchapp/views.py
+++ binding_searchapp/views.py
@@ -1,7 +1,7 @@
 # Import necessary modules
 
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Booktype, Binding
+from .models import Binding
 from .serializers import BindingSerializer
 from django.db.models import Q
 from django.views.generic import TemplateView
@@ -12,6 +12,9 @@ from elasticsearch.helpers import scan
 
 from elasticsearch_dsl import Q
 from binding_searchapp.documents import BindingDocument
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.views import APIView
+
 
 
 # Define function to display all books
@@ -27,36 +30,36 @@ def binding_detail(request, id):
     return render(request, 'binding_detail.html', {'book': book, 'type': t.btype})
 
 
-class SearchView(TemplateView):
+class SearchView(APIView, LimitOffsetPagination):
+    # entry = Entry()
     http_method_names = ['get']
     template_name = "search.html"
     title = settings.PAGE_TITLE
     drug = '*'
     target = '*'
+    count = 5
+    offset = 0
+    limit = 5
     model = Binding
     serializer_class = BindingSerializer
+    document_class = BindingDocument
     queryset = Binding.objects.all()
 
-    def test(self):
-        query = '*Br.*'
+    def generate_q_expression(self):
         q = Q(
             'bool',
             must=[
                 Q('query_string',
                   default_field='drug',
-                  query=query
-                )
+                  query=self.drug),
+                Q('query_string',
+                  default_field='target',
+                  query=self.target),
             ],
             must_not=[],
             should=[]
         )
-        search = BindingDocument.search().query(q)
-        response = search.execute()
-        print(response)
-
-        for hit in search:
-            print(hit.target)
-        return search
+        return q
 
     def set_drug_target(self, drug, target):
         if drug != '' and drug != None:
@@ -67,127 +70,30 @@ class SearchView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = self.title
-        # context["drug"] = kwargs.GET.get('drug', '*')
         return context
 
-    def es_search(self):
-        es = Elasticsearch("http://localhost:9200")
-        results = scan(es,
-                       query={
-                           "query": {
-                               "bool": {
-                                   "must":
-                                       [{
-                                           "query_string": {
-                                               "default_field": "drug",
-                                               "query": self.drug
-                                           }
-                                       }, {
-                                           "query_string": {
-                                               "default_field": "target",
-                                               "query": self.target
-                                           }
-                                       }],
-                                   "must_not": [],
-                               }
-                           }
-                       },
-                       index="binding"
-                       )
-        if results == '':
-            print('results: NOONE')
-            results = 'None'
-        # self.test()
-        # return results
-        return self.test()
-
     def get(self, request, *args, **kwargs):
-        drug = request.GET.get('drug', '*')
-        target = request.GET.get('target', '*')
+        try:
+            drug = request.GET.get('drug', '*')
+            target = request.GET.get('target', '*')
+            self.set_drug_target(drug, target)
 
-        self.set_drug_target(drug, target)
-        return render(request, 'search.html', {'drug': self.drug, 'target': self.target, 'results': self.es_search()})
+            q = self.generate_q_expression()
+            search = self.document_class.search().query(q)
+            response = search.execute()
 
-# def search(request):
-#     es = Elasticsearch("http://localhost:9200")
-#     # results = scan(
-#     #     es,
-#     #     query={
-#     #         "query": {
-#     #             "bool": {
-#     #                 "must": {"match_all": {}},
-#     #                 "must_not": [],
-#     #             }
-#     #         }
-#     #     },
-#     #     index="binding",
-#     # )
-#     if request.method == "GET":
-#         drug = request.GET.get('drug', '*')
-#         print('Drug: ', drug)
-#         if drug == '' or drug==None:
-#             drug = '*'
-#         else:
-#             drug = f'*{drug}*'
-#         target = request.GET.get('target', '*')
-#         print('Target: ', target)
-#         if target == '' or target==None:
-#             target = '*'
-#         else:
-#             target = f'*{target}*'
-#
-#
-#         print('Drug: ', drug)
-#         print('Target: ', target)
-#
-#         results = scan(
-#             es,
-#             query={
-#                 "query": {
-#                     "bool": {
-#                         "must":
-#                         [{
-#                             "query_string": {
-#                             "default_field": "drug",
-#                             "query": drug
-#                             }
-#                             },{
-#                             "query_string": {
-#                             "default_field": "target",
-#                             "query": target
-#                             }
-#                         }],
-#                         "must_not": [],
-#                     }
-#                 }
-#             },
-#             index="binding",
-#         )
-#         if results == '':
-#             print('results: NOONE')
-#             results = 'None'
-#         # results = Binding.objects.filter(Q(drug__icontains=results) | Q(target__icontains=results)
-#         # | Q(gcnnet_bindingdb_ic50__icontains=results) | Q(gcnnet_bdtdc_ic50__icontains=results)
-#         # | Q(model__icontains=results) )
-#
-#     # for result in results:
-#     #     print(result['_source']['drug'])
-#     #     # print(result)
-#
-#
-#     # resp = es.search(index="binding", query={"match_all": {}})
-#     # print("Got %d Hits:" % resp['hits']['total']['value'])
-#     # for hit in resp['hits']['hits']:
-#     #     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
-#
-#
-#     # results = []
-#
-#     # if request.method == "GET":
-#     #     query = request.GET.get('search')
-#     #     if query == '':
-#     #         query = 'None'
-#     #     results = Book.objects.filter(Q(binding_name__icontains=query) | Q(author_name__icontains=query) | Q(price__icontains=query) )
-#
-#     # return render(request, 'search.html', {'query': query, 'results': results})
-#     return render(request, 'search.html', {'drug': drug, 'target': target, 'results': results})
+            # for hit in search:
+            #     print(hit.target)
+
+            print(f'Found {response.hits.total.value} hit(s) for query: "{self.drug}"')
+
+            # results = self.paginate_queryset(response, request, view=self)
+            # serializer = self.serializer_class(results, many=True)
+
+            # self.entry.m1 = search
+            return render(request, 'search.html',
+                          {'drug': self.drug, 'target': self.target,
+                           'results': search})
+            # return self.get_paginated_response(serializer.data)
+        except Exception as e:
+            return HttpResponse(e, status=500)
